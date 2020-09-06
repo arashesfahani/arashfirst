df409cba7dd59d7770dba4721aaae67806061f23
arashesfahani <a.n.esfahani@hotmail.com>
2020-09-06 22:35:18 +0430

    rate2

--- murders.R
+++ murders.R
@@ -6,5 +6,5 @@ murders$state
 table(murders$region)
 class(murders$region)
 hist(murders$total)
-rate <-murders$total/murders$population
-rate
+rate2 <-murders$total/murders$population*100000
+rate2
